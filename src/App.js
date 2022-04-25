02c576cb81a5c910eff232411a38f6cc336d1b44
yuujinhwng <yuujin.hwng@gmail.com>
2022-04-25 01:12:30 -0400

    navicam
    camera creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ImageUpload from "./ImageUpload";
+import axios from 'axios';
+import React, { useRef, useEffect, useState } from "react";
 function App() {
+
+	const videoRef = useRef(null);
+	const photoRef = useRef(null);
+
+	const [hasPhoto, setHasPhoto] = useState(false);
+
+	const getVideo = () => {
+		navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080}})
+		.then(stream => {
+			let video = videoRef.current;
+			video.srcObject = stream;
+			video.play();
+		})
+		.catch(err => {
+			console.error(err);
+		})
+	}
+
+	const takePhoto = () => {
+		const width = 414;
+		const height = width / (16/9);
+
+		let video = videoRef.current;
+		let photo = photoRef.current;
+
+		photo.width = width;
+		photo.height = height;
+
+		let ctx = photo.getContext('2d');
+		ctx.drawImage(video, 0, 0, width, height);
+		setHasPhoto(true);
+	}
+
+	const closePhoto = () => {
+		let photo = photoRef.current;
+		let ctx = photo.getContext('2d');
+		setHasPhoto(false);
+
+		ctx.clearRect(0, 0, photo.width, photo.height);
+	}
+
+	const sendPhoto = () => {
+		const canvas= document.getElementById('my-canvas');
+		canvas.toBlob(function(blob) {
+			const newImg = document.createElement('img'),
+			url = URL.createObjectURL(blob);
+		newImg.onload = function() {
+			URL.revokeObjectURL(url);
+		};
+		newImg.src = url;
+		const ress = axios.post("http://127.0.0.1:8000/api/process", url);
+            console.log(ress);
+		});
+	}
+
+	useEffect(() => {
+		getVideo();
+	}, [videoRef])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<div className="upper">
+			<ImageUpload/>
+		</div>
+		<div className="camera">
+			<video ref={videoRef}></video>
+		  	<button onClick ={takePhoto}> SNAP!</button>
+	  	</div>
+	  	<div className={'result ' + (hasPhoto ? 'hasPhoto': '')}>
+		  	<canvas id='my-canvas' ref={photoRef}></canvas>
+		  	<button onClick ={closePhoto}>CLOSE!</button>
+			<button onClick={sendPhoto}>SEND</button>
+		</div>
+	</div>
   );
 }
 
